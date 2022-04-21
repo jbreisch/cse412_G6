cea5ce7d3a62a0e1484fcd36db3b71170d4c64a6
JP  Breisch <jbreisch@goyellowbird.com>
2022-04-21 00:09:48 -0700

    heroku

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from crypt import methods
 import imp
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_bootstrap import Bootstrap
+from requests import Session
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -13,12 +15,57 @@ db = SQLAlchemy(app)
 #def index():
 #    return render_template('index.html')
 
-@app.route('/get-flights', methods = ['POST', 'GET'])
-def getFLights():
-  return
+#DB
+class Passenger(db.Model):
+  passenger_ID = db.Column(db.Integer, db.ForeignKey('Book.passenger_ID'), primary_key=True)
+  name = db.Column(db.String(80))
+  gender = db.Column(db.String(80))
+  age = db.Column(db.Integer)
+  username = db.Column(db.String(80), unique=True)
+  password = db.Column(db.String(80))
+  email = db.Column(db.String(80))
+  phone = db.Column(db.Sting(80))
+
+class Book(db.Model):
+  ticket_ID = db.Column(db.Integer)
+  passenger_ID = db.Column(db.Integer, db.ForeignKey('Passenger.passenger_ID'), primary_key=True)
+
+class Plane_Ticket(db.Model):
+  ticket_ID = db.Column(db.Integer, db.ForeignKey('corresponds.ticket_ID'), primary_key=True)
+  plane_ID = db.Column(db.Integer)
+  departure_city = db.Column(db.String(80))
+  arrival_city = db.Column(db.String(80))
+  cost = db.Column(db.String(80))
+  departure_time = db.Column()
+  arrival_time = db.Column()
+  boarding_time = db.Column()
+
+class corresponds(db.Model):
+  ticket_ID = db.Column(db.Integer, db.ForeignKey('Seat.ticket_ID'), db.ForeignKey('Plane_Ticket.ticket_ID'), primary_key=True)
+  seat_number = db.Column(db.Integer, db.ForeignKey('Book.passenger_ID'), primary_key=True)
+
+class Seat(db.Model):
+  ticket_ID = db.Column(db.Integer,db.ForeignKey('corresponds.ticket_ID'), primary_key=True)
+  plane_ID = db.Column(db.Integer, primary_key=True)
+  seat_number = db.Column(db.Integer)
+  id = db.Column(db.Integer)
+
+class Plane(db.Model):
+  plane_ID= db.Column(db.Integer, db.ForeignKey('Seat.plane_ID'), primary_key=True)
+  type = db.Column(db.String(80))
+  name = db.Column(db.String(80))
+  num_seats = db.Column(db.Integer)
 
 
 
+
+
+
+
+
+
+
+############################################################################################
 @app.route('/', methods=['GET'])
 def home():
   """Session Control"""
@@ -44,9 +91,27 @@ def login():
           #return render_template('index.html')
           return redirect(url_for('home'))
         else:
-          return 'Dont Login'
+          #return 'Dont Login'
+          return redirect(url_for('login'))
       except:
         return "Dont Login"
+
+
+@app.route('/search_flights', methods=['GET', 'POST'])
+def search():
+  departure = request.form.get('depart')
+  arrival = request.form.get('arrive')
+  session = Session()
+  departCityList = []
+  arriveCityList = []
+  costList = []
+  departTimeList = []
+  arriveTimeList = []
+  boardingTimeList = []
+  results = session.query(PlaneTicket).all()
+  #for i in results:
+    #if ()
+
 
 
 @app.route('/home', methods=["GET"])
